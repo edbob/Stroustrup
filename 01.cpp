f172ea9a3324efadf5ade2ef58701eea3e4e51b4
edbob <1984nedov@gmail.com>
2024-01-28 00:58:38 +0200

    adCOde01

--- 01.cpp
+++ 01.cpp
@@ -3,6 +3,10 @@
 int main(){
     cout << "Heloo, World! \n";
     cout << "Here we go! \n";
+    string first_name;
+    cout << "Pleas enter first name: ";
+    cin >> first_name;
+    cout << "Hello: " << first_name << "!\n";
     keep_window_open();//Ожидает ввода символа
 
     return 0;
