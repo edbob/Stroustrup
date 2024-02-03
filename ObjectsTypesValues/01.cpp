8e4cf8b2d1c606f649ab275b0664169803268f0b
edbob <1984nedov@gmail.com>
2024-02-04 00:14:34 +0200

    addNewchanges02

--- ObjectsTypesValues/01.cpp
+++ ObjectsTypesValues/01.cpp
@@ -1,5 +1,11 @@
 #include "../std_lib_facilities.h"
 
+//39 int : целое число
+//3.5 doubl : число с пла вающей точкой
+//'.' char : отдельный символ в одинарных ка вычках
+//"Annemarie" string : последова тельность символов, ограниченная двойными кавычками!
+//true bool : либо истина , либо ложь
+
 int main(){
     cout << "Heloo, World! \n";
     cout << "Here we go! \n";
