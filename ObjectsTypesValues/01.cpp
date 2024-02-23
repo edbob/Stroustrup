4a7341eda40fda7c6cfd3786c32b42d7d08dd205
edbob <1984nedov@gmail.com>
2024-02-23 15:45:21 +0200

    addnew2302

--- ObjectsTypesValues/01.cpp
+++ ObjectsTypesValues/01.cpp
@@ -7,8 +7,6 @@
 //true bool : либо истина , либо ложь
 
 int main(){
-    cout << "Heloo, World! \n";
-    cout << "Here we go! \n";
     string first_name;
     cout << "Pleas enter first name: ";
     cin >> first_name;
